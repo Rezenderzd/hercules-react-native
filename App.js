b59d14e71d5c2e56c37cf26bf02b4b630beb333b
Rezenderzd <gmrezende07@gmail.com>
2026-02-23 21:17:08 -0300

    melhorando as cores e adicionando botoes

--- App.js
+++ App.js
@@ -1,55 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
 import { useState } from 'react';
 export default function App() {
   const [contador, setContador] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Meu Primeiro App! 🚀</Text>
-      
-      <View style={styles.contadorContainer}>
-        <Text style={styles.numero}>{contador}</Text>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Meu Primeiro App! 🚀</Text>
+
+        <View style={styles.contadorContainer}>
+          <Text style={styles.numero}>{contador}</Text>
+        </View>
+        <View style={styles.botoes}>
+          <TouchableOpacity 
+            style={styles.botao}
+            onPress={() => setContador(contador + 1)}
+          >
+            <Text style={styles.textoBotao}>➕ Aumentar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={[styles.botao, styles.botaoZerar]}
+            onPress={() => setContador(0)}
+          >
+            <Text style={styles.textoBotao}>🔄 Zerar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style={styles.botao}
+            onPress={() => setContador(contador - 1)}
+          >
+            <Text style={styles.textoBotao}>➖ Diminuir</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.botao}
+            onPress={()=>setContador(contador*2)}
+          >
+            <Text style={styles.textoBotao}>X Multiplicador</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.botao}
+            onPress={()=>setContador(contador/2)}
+          >
+            <Text style={styles.textoBotao}>: Dividir</Text>
+          </TouchableOpacity>
+        </View>
+        <StatusBar style="auto" />
       </View>
-      <View style={styles.botoes}>
-        <TouchableOpacity 
-          style={styles.botao}
-          onPress={() => setContador(contador + 1)}
-        >
-          <Text style={styles.textoBotao}>➕ Aumentar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity 
-          style={[styles.botao, styles.botaoZerar]}
-          onPress={() => setContador(0)}
-        >
-          <Text style={styles.textoBotao}>🔄 Zerar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity 
-          style={styles.botao}
-          onPress={() => setContador(contador - 1)}
-        >
-          <Text style={styles.textoBotao}>➖ Diminuir</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
-    </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#78A90B',
+    backgroundColor: '#161a1d',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+    height:100
   },
   titulo: {
     fontSize: 28,
     fontWeight: 'bold',
-    color: '#fff',
+    color: '#ba181b',
     marginBottom: 40,
   },
   contadorContainer: {
-    backgroundColor: '#FF81A4',
+    backgroundColor: '#ba181b',
     borderRadius: 20,
     padding: 40,
     marginBottom: 40,
@@ -59,24 +72,24 @@ const styles = StyleSheet.create({
   numero: {
     fontSize: 72,
     fontWeight: 'bold',
-    color: '#09AF',
+    color: '#f5f3f4',
   },
   botoes: {
     gap: 15,
     width: '100%',
   },
   botao: {
-    backgroundColor: '#4ECDC4',
+    backgroundColor: '#f5f3f4',
     padding: 18,
     borderRadius: 12,
     alignItems: 'center',
   },
   botaoZerar: {
-    backgroundColor: '#FF6B6B',
+    backgroundColor: '#ba181b',
   },
   textoBotao: {
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#0A0E27',
+    color: '#161a1d',
   },
 });
